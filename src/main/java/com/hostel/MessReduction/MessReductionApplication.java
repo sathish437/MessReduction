9be8b04f70a30eaaf7059f3ad0a0a2da23678e0e
Sathish <duraisamysathish4@gmail.com>
2026-04-01 16:35:34 +0530

    Add files via upload

---
+++ src/main/java/com/hostel/MessReduction/MessReductionApplication.java
@@ -0,0 +1,13 @@
+package com.hostel.MessReduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessReductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessReductionApplication.class, args);
+	}
+
+}

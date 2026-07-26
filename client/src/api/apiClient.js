fa2e69c06bebc538be0abb775485d0be32a924ca
Sathish <duraisamysathish4@gmail.com>
2026-07-26 15:18:34 +0530

    base url update

--- client/src/api/apiClient.js
+++ client/src/api/apiClient.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 import { getCookie } from '../utils/cookieUtils';
 
 const apiClient = axios.create({
-  // baseURL: 'https://messreduction8.onrender.com',
-  baseURL: 'http://localhost:8080',
+  baseURL: import.meta.env.VITE_API_BASE_URL || '',
   headers: {
     'Content-Type': 'application/json'
   }
